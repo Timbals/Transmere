e8a69d2e6653f919e231c395b067f3f0fad82bde
Tim Balsfulland <tim.balsfulland@gmx.de>
2016-07-19 17:37:30 +0100

    initial commit

---
+++ desktop/src/me/timbals/transmere/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.timbals.transmere.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.timbals.transmere.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
---
+++ html/src/me/timbals/transmere/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package me.timbals.transmere.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import me.timbals.transmere.Game;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Game();
+        }
+}
